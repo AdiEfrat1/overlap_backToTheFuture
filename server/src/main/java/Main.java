5d6fad1da36228e5f87729f8b8635da8a733eafd
Adi <adi@macron1.com>
2023-07-11 11:10:16 +0300

    remove(): delete the hello files

---
+++ server/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/", (req, res) -> {
+            res.redirect("Main.html");
+            return null;
+        });
+    }
+}
