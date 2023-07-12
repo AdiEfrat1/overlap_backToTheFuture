2610b90e847747f4e2ba338177d99a712c88e4b1
Adi <adi@macron1.com>
2023-07-12 11:27:35 +0300

    feat(src/main/java/BL/YoungBL): add method to search specific young full details

--- server/src/main/java/BL/YoungBL.java
+++ server/src/main/java/BL/YoungBL.java
@@ -4,6 +4,7 @@ import DAL.Young;
 import DB.YoungDB;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 public class YoungBL {
 
@@ -11,5 +12,11 @@ public class YoungBL {
 
     public ArrayList<Young> getAllYoung() {
         return this.youngDB.youngs;
+    }
+
+    public Young getSpecificFullDetails(int id) {
+        return this.youngDB.youngs.stream()
+                .filter((young) -> young.id() == id).findFirst()
+                .orElseThrow(NoSuchElementException::new);
     }
 }
