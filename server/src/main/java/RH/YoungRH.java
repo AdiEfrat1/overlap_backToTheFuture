fdeb086979361d35571185cf323e6e062e834bad
Adi <adi@macron1.com>
2023-07-12 11:50:16 +0300

    feat(src/main/java/RH/youngRH): write fullDetails get request

--- server/src/main/java/RH/YoungRH.java
+++ server/src/main/java/RH/YoungRH.java
@@ -5,6 +5,8 @@ import DAL.Young;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.NoSuchElementException;
+
 import static spark.Spark.*;
 
 public class YoungRH {
@@ -21,7 +23,18 @@ public class YoungRH {
             post("/add/:id", (req, res) -> null);
             delete("/remove/:id", (req, res) -> null);
             get("/all", (req, res) -> gson.toJson(this.youngBL.getAllYoung()));
-            get("/fullDetails/:id", (req, res) -> null);
+            get("/fullDetails/:id", (req, res) -> {
+                try {
+                    Young young = this.youngBL.getSpecificFullDetails(Integer.parseInt(req.params("id")));
+
+                    return gson.toJson(young);
+                } catch (NoSuchElementException e) {
+                    res.status(404);
+                    res.body("Resource not found");
+                }
+
+                return null;
+            });
         });
     }
 }
