2e5a899118e87c65cc9fa3cfb4f9e4da731fc42c
Adi <adi@macron1.com>
2023-07-16 17:07:23 +0300

    fix(src/main/java/RH/youngRH): create functions that are called withing the routes and do their logic

--- server/src/main/java/RH/YoungRH.java
+++ server/src/main/java/RH/YoungRH.java
@@ -4,6 +4,8 @@ import BL.YoungBL;
 import Models.Young;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import spark.Request;
+import spark.Response;
 
 import java.util.NoSuchElementException;
 
@@ -13,48 +15,73 @@ public class YoungRH {
 
     private YoungBL youngBL = new YoungBL();
 
+    private Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     public void setRoutes () {
-        GsonBuilder builder = new GsonBuilder();
-        builder.setPrettyPrinting();
-
-        Gson gson = builder.create();
 
         path("/youngs", () -> {
-            post("/add", (req, res) -> {
-                try {
-                    this.youngBL.addYoung(gson.fromJson(req.body(), Young.class));
-                } catch (Exception e) {
-                    res.status(500);
-                    res.body("Error when trying to add young");
-                }
-
-                return null;
-            });
-            delete("/remove/:id", (req, res) -> {
-                try {
-                    this.youngBL.removeYoung(Integer.parseInt(req.params("id")));
-                } catch (Exception e) {
-                    res.status(500);
-                    res.body("Error when trying to remove young");
-                }
-
-                return null;
-            });
-            get("/all", (req, res) -> gson.toJson(this.youngBL.getAllYoung()));
-            get("/:id", (req, res) -> {
-                try {
-                    Young young = this.youngBL
-                            .getSpecific(Integer.parseInt(req.params("id")));
-
-                    return gson.toJson(young);
-                } catch (NoSuchElementException e) {
-                    res.status(404);
-                    res.body("Resource not found");
-                }
-
-                return null;
-            });
+            post("/add", this::addYoung);
+            delete("/remove/:id", this::removeYoung);
+            get("/all", this::getAllYoungs);
+            get("/:id", this::getSpecificYoung);
         });
+    }
+
+    private boolean addYoung(Request req, Response res) {
+        try {
+            this.youngBL.addYoung(this.gson.fromJson(req.body(), Young.class));
+
+            return true;
+        } catch (Exception e) {
+            res.status(500);
+            res.body("Error when trying to add young");
+
+            return false;
+        }
+    }
+
+    private boolean removeYoung(Request req, Response res) {
+        try {
+            this.youngBL.removeYoung(Integer.parseInt(req.params("id")));
+
+            return true;
+        } catch (Exception e) {
+            res.status(500);
+            res.body("Error when trying to remove young");
+
+            return false;
+        }
+    }
+
+    private String getAllYoungs(Request req, Response res) {
+        res.type("application/json");
+
+        try {
+            return this.gson.toJson(this.youngBL.getAllYoung());
+        } catch (Exception e) {
+            res.status(500);
+            res.body("Error when trying to fetch all youngs");
+
+            return null;
+        }
+    }
+
+    private String getSpecificYoung(Request req, Response res) {
+        res.type("application/json");
+
+        try {
+            Young young = this.youngBL
+                    .getSpecific(Integer.parseInt(req.params("id")));
+
+            return this.gson.toJson(young);
+        } catch (NoSuchElementException e) {
+            res.status(404);
+            res.body("Resource not found");
+        } catch (Exception e) {
+            res.status(500);
+            res.body("Error when trying to fetch specific young");
+        }
+
+        return null;
     }
 }
