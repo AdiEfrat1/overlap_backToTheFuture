35864ca625331a3e241e4d483e3574b8d19a88bb
Adi <adi@macron1.com>
2023-07-16 14:57:25 +0300

    fix(src/main/java/BL/YoungBL): add exception throwing in BL methods

--- server/src/main/java/BL/YoungBL.java
+++ server/src/main/java/BL/YoungBL.java
@@ -1,32 +1,46 @@
 package BL;
 
 import DAL.Young;
-import DAL.YoungDTO;
 import DB.YoungDB;
 
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
 public class YoungBL {
 
     private YoungDB youngDB = new YoungDB();
 
-    public ArrayList<Young> getAllYoung() {
-        return this.youngDB.youngs;
+    public ArrayList<Young> getAllYoung() throws Exception {
+        try {
+            return this.youngDB.youngs;
+        } catch (Exception e) {
+            throw new Exception("Error fetcing all youngs from DB");
+        }
     }
 
-    public Young getSpecific(int id) {
-        return this.youngDB.youngs.stream()
-                .filter((young) -> young.id() == id).findFirst()
-                .orElseThrow(NoSuchElementException::new);
+    public Young getSpecific(int id) throws Exception {
+        try {
+            return this.youngDB.youngs.stream()
+                    .filter((young) -> young.id() == id).findFirst()
+                    .orElseThrow(NoSuchElementException::new);
+        } catch (Exception e) {
+            throw new Exception("Error fetcing specific young { id: " + id + " } from DB");
+        }
     }
 
-    public void removeYoung(int id) {
-        this.youngDB.youngs.removeIf((young) -> young.id() == id);
+    public void removeYoung(int id) throws Exception {
+        try {
+            this.youngDB.youngs.removeIf((young) -> young.id() == id);
+        } catch (Exception e) {
+            throw new Exception("Error removing specific young { id: " + id + " } from DB");
+        }
     }
 
-    public void addYoung(Young young) {
-        this.youngDB.youngs.add(young);
+    public void addYoung(Young young) throws Exception {
+        try {
+            this.youngDB.youngs.add(young);
+        } catch (Exception e) {
+            throw new Exception("Error add new young { id: " + young.id() + " } to DB");
+        }
     }
 }
--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -7,8 +7,6 @@ public class Main {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
-        YoungDB youngDB = new YoungDB();
-
         get("/", (req, res) -> {
             res.redirect("Main.html");
             return null;
