74283d2312115c80318ca7ad7bae06cab511433d
Adi <adi@macron1.com>
2023-07-17 15:34:22 +0300

    fix(src/main/java/DAL/YoungDAL): re-write functions to access json DB and not arrayList defined in a class

--- server/src/main/java/DAL/YoungDAL.java
+++ server/src/main/java/DAL/YoungDAL.java
@@ -2,13 +2,14 @@ package DAL;
 
 import DB.YoungDB;
 import Models.Young;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
@@ -16,42 +17,47 @@ public class YoungDAL {
 
     private final String DB_PATH = "C:/Adi Overlap/overlap_backToTheFuture/server/src/main/java/DB/youngs.json";
 
+    private Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
     private YoungDB youngDB = new YoungDB();
 
     public ArrayList<Young> getAllYoungs() throws FileNotFoundException {
         JsonElement jsonElement = new Gson().fromJson(new FileReader(this.DB_PATH), JsonElement.class);
         JsonArray jsonArray = jsonElement.getAsJsonArray();
 
-        ArrayList<Young> youngs = new ArrayList<>();
-
-        for (JsonElement element : jsonArray) {
-            JsonObject jsonObject = element.getAsJsonObject();
-
-            Young currYoung = new Young(
-                    jsonObject.get("id").getAsInt(),
-                    jsonObject.get("name").getAsString(),
-                    jsonObject.get("city").getAsString(),
-                    jsonObject.get("phone").getAsString(),
-                    jsonObject.get("hobby").getAsString(),
-                    jsonObject.get("book").getAsString());
-
-            youngs.add(currYoung);
-        }
-
-        return youngs;
+        return jsonToYoungArrayList(jsonArray);
     }
 
-    public Young getSpecific(int id) {
-        return this.youngDB.youngs.stream()
-                .filter((young) -> young.id() == id).findFirst()
-                .orElseThrow(NoSuchElementException::new);
+    public Young getSpecific(int id) throws Exception {
+        JsonElement jsonElement = new Gson().fromJson(new FileReader(this.DB_PATH), JsonElement.class);
+        JsonArray jsonArray = jsonElement.getAsJsonArray();
+
+        return jsonToYoungArrayList(jsonArray)
+                .stream()
+                .filter((young) -> young.id() == id)
+                .findFirst()
+                .orElseThrow(Exception::new);
     }
 
-    public void removeYoung(int id) {
+    public void removeYoung(int id) throws FileNotFoundException {
         this.youngDB.youngs.removeIf((young) -> young.id() == id);
     }
 
-    public void addYoung(Young young) {
-        this.youngDB.youngs.add(young);
+    public void addYoung(Young young) throws FileNotFoundException {
+        JsonElement jsonElement = new Gson().fromJson(new FileReader(this.DB_PATH), JsonElement.class);
+        JsonArray jsonArray = jsonElement.getAsJsonArray();
+        JsonObject newElement = gson.toJsonTree(young).getAsJsonObject();
+        jsonArray.add(newElement);
+
+        try (FileWriter fileWriter = new FileWriter(this.DB_PATH)) {
+            fileWriter.write(jsonArray.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private ArrayList<Young> jsonToYoungArrayList(JsonArray jsonArray) {
+        Type listType = new TypeToken<ArrayList<Young>>(){}.getType();
+        return this.gson.fromJson(jsonArray, listType);
     }
 }
