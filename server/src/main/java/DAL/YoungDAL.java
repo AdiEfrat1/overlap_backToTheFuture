2e790d31bc27611d840041392c5ee6fe08a293d9
Adi <adi@macron1.com>
2023-07-17 12:26:20 +0300

    feat(src/main/java/DB/youngs.json): create json DB file for youngs

--- server/src/main/java/DAL/YoungDAL.java
+++ server/src/main/java/DAL/YoungDAL.java
@@ -2,16 +2,43 @@ package DAL;
 
 import DB.YoungDB;
 import Models.Young;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 public class YoungDAL {
 
+    private final String DB_PATH = "C:/Adi Overlap/overlap_backToTheFuture/server/src/main/java/DB/youngs.json";
+
     private YoungDB youngDB = new YoungDB();
 
-    public ArrayList<Young> getAllYoungs() {
-        return this.youngDB.youngs;
+    public ArrayList<Young> getAllYoungs() throws FileNotFoundException {
+        JsonElement jsonElement = new Gson().fromJson(new FileReader(this.DB_PATH), JsonElement.class);
+        JsonArray jsonArray = jsonElement.getAsJsonArray();
+
+        ArrayList<Young> youngs = new ArrayList<>();
+
+        for (JsonElement element : jsonArray) {
+            JsonObject jsonObject = element.getAsJsonObject();
+
+            Young currYoung = new Young(
+                    jsonObject.get("id").getAsInt(),
+                    jsonObject.get("name").getAsString(),
+                    jsonObject.get("city").getAsString(),
+                    jsonObject.get("phone").getAsString(),
+                    jsonObject.get("hobby").getAsString(),
+                    jsonObject.get("book").getAsString());
+
+            youngs.add(currYoung);
+        }
+
+        return youngs;
     }
 
     public Young getSpecific(int id) {
